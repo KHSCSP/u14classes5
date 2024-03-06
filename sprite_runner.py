9a26e07f27c2fee922f4dac7ed4c81d897c9843c
heyscottn <heyscottn@gmail.com>
2024-03-06 13:54:32 -0600

    mm

--- sprite_runner.py
+++ sprite_runner.py
@@ -6,11 +6,16 @@ screen = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello World!')
 
 # load a background image
-bg = pygame.image.load("u14SOLNS/u14classes3SOLNprev/lighting.jpg")
+bg = pygame.image.load("u14/u14classes5/lighting.jpg")
 
 # a custom class that uses an image
 from sprite_class import Sprite
 s = Sprite(50, 50, 50)
+
+# setup the font
+my_font = pygame.font.Font('freesansbold.ttf', 12)
+timer = 0
+tick = 0
 
 while True:
     for event in pygame.event.get():
@@ -24,6 +29,17 @@ while True:
 
     # draw the sprite
     s.draw(screen)
+
+
+    # count ticks
+    tick += 1
+    # adjust timer & ticks
+    if tick >= 250: # related to the delay value
+        timer += 1
+        tick = 0 # don't allow ticks to get too large
+    # draw the text
+    text = my_font.render('Time: ' + str(timer), True, (0,0,0))
+    screen.blit(text, (0,0))
 
     # update the screen and pause
     pygame.display.update()
