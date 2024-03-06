be225dcfb207aabbac4b05d8beb949eeabb1fd12
heyscottn <heyscottn@gmail.com>
2024-03-06 13:30:17 -0600

    mm

--- bullet_class.py
+++ bullet_class.py
@@ -2,22 +2,13 @@ import pygame, math
 
 class Bullet:
     def __init__(self, xp, yp):
-        self.x = xp
-        self.y = yp
-        self.size = 5
-        self.yv = -3
-        self.color = (0,0,0)
+        pass
     
     def draw(self, screen):
-        pygame.draw.line(screen, self.color, (self.x, self.y), (self.x, self.y+self.size))
+        pass
 
     def move(self):
-        self.y += self.yv
+        pass
 
     def check_hit(self, enemies):
-        for e in enemies[:]:
-            dist = math.sqrt((self.x-e.x)**2 + (self.y-e.y)**2)
-            if dist <= e.size:
-                enemies.remove(e)
-                return True
-        return False
+        pass
--- runner.py
+++ runner.py
@@ -10,15 +10,13 @@ screen = pygame.display.set_mode((w, h))
 pygame.display.set_caption('Hello World!')
 
 # define one player, a list of 'bullets'
-p1 = Bouncy(size=20,col=(255,0,0))
+
 
 # define a list of bullets
-bullets = []
+
 
 # define a list of enemies
-enemies = []
-for i in range(10):
-    enemies.append(Bouncy(size=20))
+
 
 
 while True:
@@ -29,35 +27,27 @@ while True:
         # a bit slower keypress (not continuous)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                bullets.append(Bullet(p1.x, p1.y))
-                print(len(bullets))
+                pass
     # -------- end event loop
     screen.fill((255,255,255))
 
     
     # move and draw player
-    if pygame.MOUSEMOTION:
-        p1.x, p1.y = pygame.mouse.get_pos()
-    p1.draw(screen)
+
+
 
     # bullets - move and draw
     # iterate through copy!! (for removal from list)
-    for b in bullets[:]:
-        if b.y > 0:  
-            b.move()
-            b.draw(screen)
-        else:
-            bullets.remove(b)
+
+
 
     # enemies move and draw
-    for e in enemies:
-        e.move(screen)
-        e.draw(screen)
+
+
 
     # check for collision
-    for b in bullets[:]:
-        if b.check_hit(enemies):
-            bullets.remove(b)
+
+
                 
     
     pygame.display.update()
